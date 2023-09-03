4e103cda23b6c6965243c787f61f882ce7329536
ihakhverdiyev2019 <53315824+ihakhverdiyev2019@users.noreply.github.com>
2023-09-03 12:25:32 +0200

    update serve static

--- src/main/java/az/elixir/experise/config/SpringWebConfig.java
+++ src/main/java/az/elixir/experise/config/SpringWebConfig.java
@@ -1,18 +1,17 @@
-package az.elixir.experise.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@EnableWebMvc
-@Configuration
-@ComponentScan({"az.elixir.experise"})
-public class SpringWebConfig implements WebMvcConfigurer {
-
-  @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-  }
-}
+//package az.elixir.experise.config;
+//
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@EnableWebMvc
+//@Configuration
+//public class SpringWebConfig implements WebMvcConfigurer {
+//
+//  @Override
+//  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//    registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+//  }
+//}
